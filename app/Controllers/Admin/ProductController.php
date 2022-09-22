3079e4b200dc1dd93793d94cbd51056634916b08
Ridwan Ibrahim <ridwanmiharbi@gmail.com>
2022-09-22 22:43:42 +0700

    first commit

---
+++ app/Controllers/Admin/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+
+class ProductController extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Product List'
+        ];
+        
+        return view('admin/product/index', $data);
+    }
+}
